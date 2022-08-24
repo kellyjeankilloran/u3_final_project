a09cf6a5f228b0742b5bf23a8c455f6915a7f99b
Kelly Killoran <kellyjeankilloran@gmail.com>
2022-08-23 21:31:59 -0700

    nav bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import meditations from './data/meditations.js'
 const App = () => {
   return (
     <div className="App">
+      <nav>
+        <section>
+          <div>HOME</div>
+          <div>ABOUT</div>
+          <div>CLASSES</div>
+        </section>
+        <div>LOG OUT</div>
+      </nav>
       <h1>Op e n Meditation Archive</h1>
       {meditations.map((meditation) => (
         <Meditation key={meditation.id} meditation={meditation} />
