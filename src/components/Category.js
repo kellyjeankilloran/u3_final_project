2c7812eb10cdae07c5a77da1ebad29cee35b12a9
Kelly Killoran <kellyjeankilloran@gmail.com>
2022-08-23 22:21:24 -0700

    CSS

---
+++ src/components/Category.js
@@ -0,0 +1,24 @@
+const Category = (props) => {
+  let Color
+  switch (props.name) {
+    case 'Inspire':
+      Color = '#AAAE8E'
+      break
+    case 'Calm':
+      Color = '#FF0022'
+      break
+    case 'Ground':
+      Color = '#41EAD4'
+      break
+    case 'Energize':
+      Color = '#5C2751'
+      break
+    default:
+      Color = 'gray'
+  }
+  return (
+    <div style={{ backgroundColor: Color, color: 'white' }}>{props.name}</div>
+  )
+}
+
+export default Category
--- src/components/Meditation.js
+++ src/components/Meditation.js
@@ -1,12 +1,24 @@
+import Category from './Category'
+
 let Meditation = (props) => {
   return (
     <div>
       <div key={props.meditation.id}>
-        <img src={props.meditation.cover_image} />
+        <img
+          src={props.meditation.cover_image}
+          alt="logo for each recorded meditation"
+        />
         <h2>{props.meditation.title}</h2>
-        <ul>{props.meditation.teacher}</ul>
-        <ul>{props.meditation.length}</ul>
+        <ul className="info">{props.meditation.teacher}</ul>
+        <ul className="info">{props.meditation.length}</ul>
       </div>
+      {props.meditation.category.map((category) => (
+        <Category
+          className="cat_block"
+          key={category.id}
+          name={category.name}
+        />
+      ))}
     </div>
   )
 }
